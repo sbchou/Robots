b0a3255aadeb1d8d5abfe7d4f44e57a20de757a7
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2013-09-19 21:15:41 -0400

    basic example

--- wall_follower.m
+++ wall_follower.m
@@ -1,2 +1,23 @@
-function hit_wall = bumpCheckReact(serPort)
-  
+function dist = wall_follower(serPort)
+  v= 0.5;
+  w= 0;
+  SetFwdVelAngVelCreate(serPort,v,w);
+
+  distance_travelled = 0;
+  bumped = false;
+
+  while ~bumped
+    bumped = hit_wall(serPort);
+    distance_travelled = distance_travelled + DistanceSensorRoomba(serPort);
+    pause(0.1)
+  end
+  v = 0
+  w = 0
+  SetFwdVelAngVelCreate(serPort, v,w);
+  dist = distance_travelled;
+end
+
+function hit_wall = bump_check(serPort)
+  [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster BumpFront] = BumpsWheelDropsSensorsRoomba(serPort);
+  hit_wall = BumpRight || BumpLeft || BumpFront;
+end
