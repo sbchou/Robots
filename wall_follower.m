bc530d6b8f36c128e880de03b6babb6b70d5eb04
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2013-09-24 23:55:29 -0400

    some changes

--- wall_follower.m
+++ wall_follower.m
@@ -1,3 +1,20 @@
+% function [wall_sensor]   = AnalogWallSensorReadRoomba(serPort)
+%   try
+%     set(serPort,'timeout',0.01);
+%     %Flush buffer
+%     N = serPort.BytesAvailable();
+%     while(N~=0) 
+%       fread(serPort,N);
+%       N = serPort.BytesAvailable();
+%     end
+%   catch
+%   end
+
+%   %% Get (142) Wall Reading(8) data fields
+%   fwrite(serPort, [142 27]);
+%   wall_sensor = fread(serPort, 1, 'uint16');
+% end
+
 %roomba moves ccwise
 function dist = wall_follower(serPort)
   v= 0.2;
@@ -6,35 +23,35 @@ function dist = wall_follower(serPort)
 
   distance_travelled = 0;
   bumped = false;
-  t_start = tic;
-  max_time = 120;
   prev_bump = false;
 
-  while toc(t_start) < max_time
+  while true
     bumped_side = bump_check(serPort);
-    
     if bumped_side 
       if ~prev_bump
         %turn to the right without moving until right sensor is triggered
         right = false
+        % stop
+        SetFwdVelAngVelCreate(serPort, 0, 0); % stop
         while ~right
-          turnAngle(r,0.1, -10)
-          [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster BumpFront] = BumpsWheelDropsSensorsRoomba(serPort)
-          right = BumpRight
+          turnAngle(r,0.1, -10) % turn right until sensor is triggered
+          bumped = bump_check(serPort)
+          right = bumped == 'right'
           pause(0.1)
         end
+        SetFwdVelAngVelCreate(serPort, 0.2, 0); % continue forward
       else %no prev bump
         if bumped_side == 'right' || bumped_side == 'left'
           prev_bump = bumped_side;
           % continue straight because side is active 
-          travelDist(serPort, 0.5, 0.1)
+          SetFwdVelAngVelCreate(serPort,0.2,0);
         else
-          % front sensor has been triggered, turn away from the side that you were traveling along before
-          right = false        
+          % front sensor has been triggered, turn towards the right
+          right = false
           while ~right
-            turnAngle(serPort,0.1, 10)
-            [BumpRight BumpLeft WheDropRight WheDropLeft WheDropCaster BumpFront] = BumpsWheelDropsSensorsRoomba(serPort)
-            right = BumpRight
+            turnAngle(serPort, 0.1, 10)
+            bumped = bump_check(serPort)
+            right = bumped == 'right'
             pause(0.1)
           end
         end
@@ -43,8 +60,12 @@ function dist = wall_follower(serPort)
       % no sensors triggered. if prev_bump is initialized, turn in the direction of the previous sensor and then go forward. If not, just continue.
         if prev_bump == 'right'
           turnAngle(serPort, 0.2, -10)
-        else
+          SetFwdVelAngVelCreate(serPort, 0.2, 0); % continue forward
+        elseif prev_bump == 'left'
           turnAngle(serPort, 0.2, 10)
+          SetFwdVelAngVelCreate(serPort, 0.2, 0); % continue forward
+        else
+          SetFwdVelAngVelCreate(serPort, 0.2, 0); % continue forward
         end
     end 
     distance_travelled = distance_travelled + DistanceSensorRoomba(serPort);
